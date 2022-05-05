40344e9898d0a15f7c0bd2daa686a2010e0f076f
frog1123 <93895449+frog1123@users.noreply.github.com>
2022-05-04 21:09:40 -0500

    .

--- js/important/ui.js
+++ js/important/ui.js
@@ -1,3 +1,5 @@
+var activeTab = 1;
+
 window.onload = () => {
     showUnlocked();
     updateUI();
@@ -7,9 +9,11 @@ function updateUI() {
     document.getElementById("frog-amount-txt").innerHTML = `${fv(player.frogAmount)} Frogs`;
     document.getElementById("frog-gain-txt").innerHTML = `Gaining ${fv(player.ponds.frog.tier1.lvl.mul(player.ponds.frog.tier1.mult))} Frog${player.ponds.frog.tier1.lvl.mul(player.ponds.frog.tier1.mult).neq('1') ? "s" : ""}/s`;
 
-    updatePondsUI();
-    updatePondMultiplierUI();
-    updateRiverUI();
+    if (activeTab == 1) {
+        updatePondsUI();
+        updatePondMultiplierUI();
+        updateRiverUI();
+    };
 
     setTimeout(updateUI, player.settings.uiUpdateSpeed);
 };
@@ -22,6 +26,9 @@ function updatePondsUI() {
     updatePondUI(5);
     updatePondUI(6);
 };
+
+document.getElementById("page-1-btn").onclick = () => switchTab(1);
+document.getElementById("page-2-btn").onclick = () => switchTab(2);
 
 document.getElementById("pond-multiply-buy-btn").onclick = () => buyPondMultiplier();
 document.getElementById("pond-multiply-buy-max-btn").onclick = () => buyPondMultiplierMax();
@@ -44,4 +51,11 @@ function showUnlocked() {
     if (player.ponds.frog.tier4.unlocked) document.getElementById("pond-t4-div").style.display = "grid";
     if (player.ponds.frog.tier5.unlocked) document.getElementById("pond-t5-div").style.display = "grid";
     if (player.ponds.frog.tier6.unlocked) document.getElementById("pond-t6-div").style.display = "grid";
+};
+function switchTab(tab) {
+    document.getElementById("page-1").style.display = "none";
+    document.getElementById("page-2").style.display = "none";
+
+    activeTab = tab;
+    document.getElementById(`page-${tab}`).style.display = "block";
 };
