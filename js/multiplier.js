f8ac6dabaf134eb2116c19cd3a2058de358e6367
frog1123 <93895449+frog1123@users.noreply.github.com>
2022-05-07 14:27:30 -0500

    stuff

--- js/multiplier.js
+++ js/multiplier.js
@@ -6,8 +6,7 @@ function updatePondMultiplierUI() {
     cost = new Decimal((Decimal.floor(Decimal.log10(Decimal.fromComponents(1, player.ponds.frog.multiplier.cost.layer, player.ponds.frog.multiplier.cost.mag))))); // cost
     costIncrease = new Decimal(Decimal.log10(player.ponds.frog.multiplier.costIncrease));
     
-    buying = Decimal.floor(amount.sub(cost).div(costIncrease))
-            .add(player.ponds.frog.multiplier.costIncrease.gt('10') ? '1' : player.ponds.frog.multiplier.costIncrease.lt('10') ? '-1' : '0') // if costincrease doesnt eq 10
+    buying = Decimal.floor(amount.sub(cost).div(costIncrease));
 
 
     document.getElementById("pond-multiply-buy-btn-txt").innerHTML = `Cost: ${fv(player.ponds.frog.multiplier.cost)}`;
@@ -50,9 +49,19 @@ function buyPondMultiplierMax() {
     };
     if (player.frogAmount.gte(player.ponds.frog.multiplier.cost)) {
         if (buying.gt('0')) {
-            player.frogAmount = player.frogAmount.sub(Decimal.pow(player.ponds.frog.multiplier.costIncrease, buying));
+            var costIncrease = player.ponds.frog.multiplier.costIncrease;
+
+            player.frogAmount = player.frogAmount.sub(Decimal.pow(player.ponds.frog.multiplier.costIncrease, buying)
+            .add(Decimal.pow(player.ponds.frog.multiplier.costIncrease, buying).mul(Decimal.pow(costIncrease, '2')))
+            .add(Decimal.pow(player.ponds.frog.multiplier.costIncrease, buying).mul(costIncrease))
+            .add(Decimal.pow(player.ponds.frog.multiplier.costIncrease, buying).div(costIncrease))
+            .add(Decimal.pow(player.ponds.frog.multiplier.costIncrease, buying).div(Decimal.pow(costIncrease, '2'))));
+
+
             player.ponds.frog.multiplier.cost = player.ponds.frog.multiplier.cost.mul(Decimal.pow(player.ponds.frog.multiplier.costIncrease, buying));
             player.ponds.frog.multiplier.lvl = player.ponds.frog.multiplier.lvl.mul(Decimal.pow(player.ponds.frog.multiplier.effectiveness, buying));
+
+            buyPondMultiplier();
         };
         buyPondMultiplier();
     };
